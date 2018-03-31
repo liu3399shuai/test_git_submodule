6c45e300d774bdcf0b50ff11e50861ce6bfe7d6e
liushuai <liu3399shuai@163.com>
2018-03-31 10:57:35 +0800

    s

---
+++ git_submodule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git_submodule
+//
+//  Created by liu on 2018/3/31.
+//  Copyright © 2018年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
